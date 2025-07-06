fa7436c0af554369b588a954e5a70489a768e17a
rajkumar <yadavraj70040@gmail.com>
2025-07-06 17:04:15 +0530

    initial commit

---
+++ My-PortFolio-main/src/assets/assets.js
@@ -0,0 +1,2 @@
+import myPhoto from "./images/passportSizePhoto.jpeg";
+export default myPhoto;
---
+++ My-PortFolio-main/src/data/constants.js
@@ -0,0 +1,320 @@
+export const Bio = {
+  name: "Rajkumar Yadav",
+  roles: ["Full Stack Web Developer", "Java  Developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/rajkumar70040",
+  //Resume ka link kam nahi kar raha h
+  resume:"https://drive.google.com/file/d/1J8A41rXzwkbycLPPz9EQl5LFxHStfd2X/view?usp=drive_link",
+
+  linkedin: "https://www.linkedin.com/in/rajkumaryadav12/",
+  twitter: "https://twitter.com/rajkumar yadav",
+  insta: "https://www.instagram.com/rajkumar_1961/",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Taiwind CSS",
+        image:
+          "https://res.cloudinary.com/dbgvjxepr/image/upload/v1731104880/tailwind_wft9uq.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "JSE ",
+        image: "https://ritjaipur.in/wp-content/uploads/2024/02/Core-Java-Pro.jpg",
+      },
+      {
+        name: "J2E ",
+        image: "https://finvoc.com/images/javaj2ee.png",
+      },
+      
+        
+      {
+        name: "Spring ",
+        image:
+          "https://i0.wp.com/e4developer.com/wp-content/uploads/2018/01/spring-boot.png?w=1300&ssl=1",
+      },
+      {
+        name: "Oracle",
+        image:
+          "https://www.inventateq.com/top-stories/wp-content/uploads/2024/05/oracle2.png.webp",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "ECLIPSE",
+        image:
+          "https://res.cloudinary.com/dpodwu487/image/upload/v1740671983/EC_qeptfg.png",
+      },
+      {
+        name: "ORACLE",
+        image: "https://res.cloudinary.com/dpodwu487/image/upload/v1740671990/ORACLE_fs0trc.png",
+      },
+    ],
+  },
+];
+
+export const trainings = [
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740671241/nit_jn2rho.png",
+    company: "NARESH IT",
+    date: "Aug 2024 - JUNE 2025",
+    desc: "During the 10-month training period (Aug 2024 -JUNE 2025), I completed Full Stack Web Development",
+    //java certificate add krna hai
+    skills: [
+      "Java Full Stack",
+    ],
+    doc: "https://res-console.cloudinary.com/ds1bzcnz2/thumbnails/v1/image/upload/v1750613208/TmFyZXNoSXRfQ3J0ZmN0X2t1aWV2eQ==/drilldown",
+  },
+  {
+    id: 1,
+    img: "https://cdn-images-1.medium.com/max/1200/1*eHdalKXpZrnlIkps0E2meA.png",
+    company: "Physics Wallah (PW)",
+    date: "September 2023 - Feb 2024",
+    desc: "During the 6-month Learning period (September 2023 - Feb 2024), I completed Data Structure Algolithm",
+    skills: [
+      "Data Structure Algorithm ",
+    ],
+    doc: "https://media.licdn.com/dms/image/v2/D4D22AQG29dc4F21LLQ/feedshare-shrink_1280/feedshare-shrink_1280/0/1707141186328?e=1753315200&v=beta&t=3v-diWBeyovrYG0ieDfE9oOWQ1kUndGX__4SIK1ALnw",
+  },
+  {
+    id: 0,
+    img: "https://internshala-uploads.internshala.com/logo/16i9letsb34-21948.png.webp",
+    company: "Codec Technologies",
+    date: "June 2025 - September 2025",
+    desc: "During this 3-Month virtual internship, I completed  of java Developer Internship, I gained hands-on experience .",
+    skills: [
+      "Java , Servlet ,JSP, Spring Boot",
+    ],
+    doc: "https://res-console.cloudinary.com/ds1bzcnz2/thumbnails/v1/image/upload/v1750615705/T2ZmZXItTGV0dGVyLV9SYWprdW1hcl9ZYWRhdl9wYWdlLTAwMDFfcGltNzJs/drilldown",
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "",
+    company: "",
+    date: "",
+    desc: "",
+    skills: [],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740670711/Patel-College-of-Science-and-Technology-Bhopal1_xewkvm.webp",
+    school: "Patel college of Science and Technology, Bhopal",
+    date: "Jun 2021 - Jul 2025",
+    grade: "7.65 CGPA",
+    desc: "I am completed a Bachelor's degree in Computer Science and Engineering at Patel college of Science and Technology, Bhopal. I have completed 8 semesters and have a CGPA of 7.65. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - B.Tech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://zilaschoolhazaribag.com/images/banner_1",
+    school: "+2 Zila School Hazaribagh",
+    date: "Apr 2019 - Feb 2021",
+    grade: "68.60%",
+    desc: "I completed my class 12 high school education at +2 Zila School Hazaribagh, where I studied Science with maths.",
+    degree: "ISC(XII), Science with Maths",
+  },
+  {
+    id: 0,
+    img: "https://tse3.mm.bing.net/th?id=OIP.Jg2LWDThG5hMPPYTvMPpEwHaFj&pid=Api&P=0&h=180",
+    school: "Utkramit High School Jamdar , Giridih",
+    date: "Apr 2018 - Apr 2019",
+    grade: "85.80%",
+    desc: "I completed my class 10 education at Utkramit High School Jamdar , Giridih, where I studied various subjects including Mathematics, Science, Social Science, English, and Hindi.",
+    degree: "Matriculation",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 3,
+    title: "Smart Contact Manager",
+    date: "Jan 2025 - Feb 2025",
+    description:
+      "Hello everyone ! I am feeling happy to share that with you all that I have developed: A Smart Contact Manager app developed with Core Java, Spring Boot, Thymeleaf and MySQL streamlines student, faculty, and administrative tasks. Utilizing Java's object-oriented approach.",
+    image:
+      "https://user-images.githubusercontent.com/47785309/142222914-08795ecc-c8f4-4d82-9605-65c448804f10.png",
+    tags: ["Core java ","Spring Boot","MYSQL"],
+    category: "web app",
+    github: "https://github.com/rajkumar70040/Smart-Contact-Manager",
+    webapp: "'",
+    member: [
+      {
+        name: "Rajkumar Yadav",
+     
+        img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669630/IMG-20250227-WA0000_oqss5h.jpg",
+        linkedin: "",
+        github: "https://github.com/rajkumar70040/Smart-Contact-Manager",
+      },
+    ]
+  },
+
+  // {
+  //   id: 2,
+  //   title: "Bank Management System",
+  //   date: "Aug 2024 - Sep 2024",
+  //   description:
+  //     "Hello everyone ! I am feeling happy to share that with you all that I have developed: A University Management System app developed with Core Java, JDBC, and MySQL streamlines student, faculty, and administrative tasks. Utilizing Java's object-oriented approach,.",
+  //   image:
+  //     "https://i0.wp.com/www.iedunote.com/img/1087/bank-management.jpg",
+  //   tags: ["Core java", "JDBC", "Oracle"],
+  //   category: "web app",
+  //   github: "https://github.com/sandeepkrmehta/Infosten",
+  //   webapp: "https://infosten.netlify.app/",
+  //   member: [
+  //     {
+  //       name: "Sanjay Kumar Mahto",
+  //       img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669760/IMG_20231217_000002_470_gyvhgr.jpg",
+  //       linkedin: "https://www.linkedin.com/in/sandeep-kumar-mehta/",
+  //       github: "https://github.com/sandeepkrmehta",
+  //     },
+  //   ]
+  // },
+
+  {
+    id: 1,
+    title: "Weather Forecast",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "The Weather Forecast Clone is a replica of popular weather applications, offering a user-friendly and accurate weather experience. It provides real-time weather updates, hourly and weekly forecasts, and interactive weather maps.",
+    image:
+      "https://play-lh.googleusercontent.com/P18rfcRRIkBWXN3avfE7byGfgvcFwgfitCAkc4C_VTW5BHxq6UBVyR9fII725Kswz04",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/rajkumar70040/Weather-App-Test-",
+    webapp: "",
+    member: [
+      {
+        name: "Rajkumar Yadav",
+        // apna img dena hai
+       img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669630/IMG-20250227-WA0000_oqss5h.jpg",
+        linkedin: "https://www.linkedin.com/in/rajkumaryadav12/",
+        github: "https://github.com/rajkumar70040/Weather-App-Test-",
+      },
+    ]
+  },
+  {
+    id: 0,
+    title: "Hotel Management  System",
+    date: "Oct 2024 - Nov 2024",
+    description:
+      "The Front Desk Hotel Management System is a GUI-based project developed in java, designed specifically for managing front desk operations in a hotel environment. This system enables streamlined management of guest check-ins, check-outs, and reservations, tailored for efficient service delivery at the front desk. Key functionalities include user registration, login, and booking management. It securely handles client details such as name, reservation number, contact information, and room preferences, ensuring a smooth, organized guest experience from arrival to departure.",
+    image:
+      "https://res.cloudinary.com/dbgvjxepr/image/upload/v1731102356/Hotel_h0ctuz.jpg",
+    tags: ["Java, MySQL"],
+    category: "gui application",
+    github: "https://github.com/Sanjayk32072/Hotel-management-system.github.io",
+    webapp: "",
+    member: [
+      {
+
+        
+        name: "Sanjay Kumar Mahto",
+        // apna img dena hai
+       img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669760/IMG_20231217_000002_470_gyvhgr.jpg",
+        linkedin: "https://www.linkedin.com/in/sanjay-kumar-mahto-605956255/",
+        github: "https://github.com/Sanjayk32072/Film-Fusion",
+      },
+    {
+        name: "Sanjeet kumar ",
+        // sanjeet pic
+        img: "",
+        linkedin: "https://www.linkedin.com/in/sanjeet-kumar-722478258/",
+        github: "",
+      },
+      {
+        name: "Rajkumar yadav",
+        //raj pic
+        img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669630/IMG-20250227-WA0000_oqss5h.jpg",
+        linkedin: "https://www.linkedin.com/in/rajkumaryadav12/",
+        github: "https://github.com/rajkumar70040",
+      },
+      {
+        name: "Rajesh kumar",
+        
+        img: "https://res.cloudinary.com/dpodwu487/image/upload/v1740669631/IMG-20250227-WA0001_akxjcg.jpg",
+        linkedin:" https://www.linkedin.com/in/rajesh-kumar-mahto01012001/",
+        github: "",
+      },
+      
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey" },
+  { year: 2022, text: "Worked as a front-end developer" },
+  { year: 2023, text: "Founded JavaScript Mastery" },
+  { year: 2024, text: "Shared my projects with the world" },
+  { year: 2025, text: "Started my own platform" },
+];
